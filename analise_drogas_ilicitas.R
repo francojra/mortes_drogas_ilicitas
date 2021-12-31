f7f21c99f9e42c9f0f7728ff98543e668ca5ecbe
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2021-12-30 23:45:33 -0300

    Script

---
+++ analise_drogas_ilicitas.R
@@ -0,0 +1,18 @@
+
+# Uso de drogas ilícitas no mundo ----------------------------------------------------------------------------------------------------------
+# Autora: Jeanne Franco --------------------------------------------------------------------------------------------------------------------
+# Data: 30/12/21 ---------------------------------------------------------------------------------------------------------------------------
+
+# Carregar dados ---------------------------------------------------------------------------------------------------------------------------
+
+dados <- read.csv("deaths-drug-overdoses.csv", sep =  ",", dec = ".")
+View(dados)
+names(dados) # Função para verificar todos os nomes das variáveis
+
+# Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
+
+# Drogas ilícitas são drogas que foram proibidas sob tratados internacionais de controle de drogas.
+# Os prinicpais grupos de drogas ilícitas usados nas estatísticas são opioides, cocaína, anfetaminas
+# e cannabis.
+
+# 
